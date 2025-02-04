0abe17ecf4e8b6c7ca808fa10c57b669602ff360
Astelen <astelen@hotmail.com>
2025-02-04 17:35:29 +0100

    Modifiche esercizio computer (h.17;35)

--- draft_esercizio.py
+++ draft_esercizio.py
@@ -1,28 +1,45 @@
-class Veicolo:
-    def __init__(self, marca, modello):
-        self.marca = marca
-        self.modello = modello
-    def mostra_informazioni(self):
-        print(f"Veicolo marca {self.marca}, modello {self.modello}")
-
-class DotazioniSpeciali:
-    def __init__(self, dotazioni):
-        self.dotazioni = dotazioni
-    def mostra_dotazioni(self):
-        print(f"Dotazioni speciali: {self.dotazioni}")
-        
-class AutomobileSportiva(Veicolo, DotazioniSpeciali):
-    def __init__(self, marca, modello, dotazioni, cavalli):
-        Veicolo.__init__(self, marca, modello) #richiama questi parametri dalla classe padre Veicolo
-        DotazioniSpeciali.__init__(self, dotazioni) #richiama questi parametri dalla classe padre DotazioniSpeciali
-        self.cavalli = cavalli #aggiunge questo parametro
+#classe computer che ha come padre processore (velocita') e RAM (memoria)
+class Processore:
+    def __init__(self, tipo_processore, velocita):
+        self.tipo_processore = tipo_processore
+        self.velocita = velocita
     
-    def mostra_informazioni(self):
-        super().mostra_informazioni()  ##prende da prima classe padre ovvero Veicolo
-        print(f"Potenza: {self.cavalli} CV")
-        self.mostra_dotazioni()
+    def sistema_supportato():
+        print("I nuovi processori supportano solo sistemi operativi rilasciati dal 2020 in poi.")
         
-autosportiva1 = AutomobileSportiva("BMW", "5", "Cruise control", "1500")
-autosportiva1.mostra_informazioni()
-
-
+    def stampa_info_processore(self):
+        print("Le informazioni del processore sono: ", self.tipo_processore, self.velocita)
+    
+class Ram:
+    def __init__(self, memoria):
+        self.memoria = int(memoria)
+    
+    def aumento_ram(self):
+        if self.memoria < 100:
+            print("Questa RAM puo' essere estesa, devi acquistare un'estensione!")
+            
+    def stampa_info_ram(self):
+        print("Le informazioni della ram sono: ", self.memoria)
+    
+class Computer(Processore, Ram):
+    def __init__(self, tipo_processore, velocita, memoria, linguaggio_computer):
+        Processore.__init__(self, tipo_processore, velocita)
+        Ram.__init__(self, memoria)
+        self.linguaggio_computer = linguaggio_computer
+       
+    def tipo_tastiera(self):
+        if self.linguaggio_computer == "eng":
+            print("La tastiera ha la formattazione inglese")
+        elif self.linguaggio_computer == "it":
+            print("La tastiera ha la formattazione inglese ")
+        else:
+            print("Formattazione non riconosciuta o inserimento non valido.")
+            
+    def stampa_informazioni(self):
+        Processore.stampa_info_processore(self)
+        Ram.stampa_info_ram(self)
+        
+            
+computer1 = Computer("Intel", "Super", "100", "eng")
+computer1.stampa_informazioni()
+computer1.tipo_tastiera()
